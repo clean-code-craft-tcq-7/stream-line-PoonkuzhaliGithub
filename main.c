ee023e60352bd6e4b4c9e18967e42eae2070a9b6
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-18 13:41:23 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Sensor.h"
 
-int i;
-for(i=0;i<50;i++)
-{
-  scanf("\n %d",&i);
-  printf("\n %d",i);
-  while(1)
+int main(){
+    int sensorData1,sensorData2;
+    FILE *fp1, *fp2;
+    fp1 = fopen("sensor_1.txt","r+");
+    fp2 = fopen("sensor_2.txt","r+");
+  
+    for(;( (!feof(fp1)) && (!feof(fp2)) );)
+    {
+        fscanf(fp1,"%d\n",&sensorData1);
+        fscanf(fp2,"%d\n",&sensorData2);
+        printf("%d\n%d***\n",sensorData1,sensorData2);
+    }
+    fclose(fp1);
+    fclose(fp2);
+return 0;
 }
