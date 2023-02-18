a54512e36834a00738dd8d42efa59054f388e6f6
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-18 13:26:49 +0530

    Update Sensor_1.c

--- Sensor_1.c
+++ Sensor_1.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define NUMBER_OF_READINGS    50
-int create_sensor_data();
+int create_sensor1_data();
 
-int create_sensor_data() 
+int create_sensor1_data() 
 {
-  int sensorData,i;
+  int sensor1Data,i;
   FILE *fp;
   fp = fopen("sensor_1.txt","w+");
   for(i=0;i<NUMBER_OF_READINGS;i++)
   {
-      sensorData=rand();
-      fprintf(fp,"%d\n",sensorData);
+      sensor1Data=rand();
+      fprintf(fp,"%d\n",sensor1Data);
   }
   fclose(fp);
   return 0;
