c53b15dea7c93fb6eb12398c83d8356951ebdbe3
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-19 11:59:57 +0530

    Update TestCases.c

--- TestCases.c
+++ TestCases.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
 #include "Sensor.h"
 #include <assert.h>
-#include <stdlib.h>
+
+void TEST_CASE_01();
+void TEST_CASE_02();
+
+void TEST_CASE_01()
+{
+  int sentData = PrintSensorData1();
+  int convertData = ConvertData1(sentData);
+
+  assert( sentData == convertData);
+}
+void TEST_CASE_02()
+{
+  int sentData = PrintSensorData2();
+  int convertData = ConvertData2(sentData);
+
+  assert( sentData == convertData);
+}
+int main()
+{
+   TEST_CASE_01(); 
+   TEST_CASE_02(); 
+}
