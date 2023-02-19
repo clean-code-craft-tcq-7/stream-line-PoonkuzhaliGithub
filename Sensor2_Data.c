48b853371eda87eb10a12bb64a11bacd2af81bb5
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-19 11:54:35 +0530

    Update and rename SenderSensor_2.c to Sensor2_Data.c

---
+++ Sensor2_Data.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Sender.h"
+
+int create_sensor2_data() 
+{
+  int sensorData;
+  FILE *fp;
+  fp = fopen("sensor_2.txt","w+");
+  for(i=0;i<NUMBER_OF_READINGS;i++)
+  {
+    sensorData=rand();
+    fprintf(fp,"%d\n",sensorData);
+  }
+  fclose(fp);
+  return 0;
+}
+
+int PrintSensorData2()
+{
+  FILE *fp2;
+  create_sensor2_data();
+  fp2 = fopen("sensor_2.txt","r+");
+  for(;(!feof(fp2));)
+  {
+    fscanf(fp2,"%d\n",&sensor2Data);
+    printf("\n%d\n",sensor2Data);   
+  }  
+  fclose(fp2);
+  return sensor2Data;
+}
