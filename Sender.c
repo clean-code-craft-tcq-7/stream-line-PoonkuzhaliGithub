146fac29efe9f44f1f302e31e2517b8bc452de85
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-19 11:59:07 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "Sender.h"
 
-int main(){
-    int sensorData1,sensorData2;
-    FILE *fp1, *fp2;
-    fp1 = fopen("sensor_1.txt","r+");
-    fp2 = fopen("sensor_2.txt","r+");
-  
-    for(;( (!feof(fp1)) && (!feof(fp2)) );)
-    {
-        fscanf(fp1,"%d\n",&sensorData1);
-        fscanf(fp2,"%d\n",&sensorData2);
-        printf("%d\n%d***\n",sensorData1,sensorData2);
-    }
-    fclose(fp1);
-    fclose(fp2);
-return 0;
+int ConvertData1(int convertedData1)
+{
+    sprintf(str,"%d\n",convertedData1);
+    sscanf(str,"%d\n",&convertedData1);
+    return convertedData1;
+}
+
+int ConvertData2(int convertedData2)
+{
+    sprintf(str,"%d\n",convertedData2);
+    sscanf(str,"%d\n",&convertedData2);
+    return convertedData2;
+}
+int SendSensorData()
+{
+   int data1, data2;
+   data1 = PrintSensorData1();
+   ConvertData1(data1);
+   data2 = PrintSensorData2();
+   ConvertData2(data2);
 }
